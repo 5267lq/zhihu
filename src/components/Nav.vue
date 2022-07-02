201cc2b6538e69f23bd11dd06f5f89f0d5401c0b
liuqi <boxingliuqi@163.com>
2022-07-02 23:54:37 +0800

    6th:PersonView-half

---
+++ src/components/Nav.vue
@@ -0,0 +1,24 @@
+<template>
+  <van-nav-bar left-text="返回" left-arrow @click-left="handle" />
+</template>
+
+<script>
+import { useRouter } from "vue-router";
+export default {
+  name: "Nav",
+  setup() {
+    let router = useRouter();
+    function handle() {
+      router.go(-1);
+    }
+    return { handle };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+/deep/ .van-icon,
+/deep/ .van-nav-bar__text {
+  color: #000;
+}
+</style>
--- src/views/PersonView.vue
+++ src/views/PersonView.vue
@@ -1,17 +1,39 @@
 <template>
-  个人中心
+  <Nav></Nav>
+  <div class="base-info">
+    <img src="@/assets/images/timg.jpg" alt="" />
+    <p>lkk</p>
+  </div>
+  <van-cell-group>
+    <van-cell title="我的收藏" is-link />
+    <van-cell value="退出登录" />
+  </van-cell-group>
 </template>
 
 <script>
-
+import Nav from "@/components/Nav.vue";
 export default {
-  name: 'PersonView',
-  components: {
-    
-  }
-}
+  name: "PersonView",
+  components: { Nav },
+};
 </script>
 
 <style lang="less" scoped>
-
+.base-info {
+  box-sizing: border-box;
+  margin: 20px 0;
+  img {
+    height: 86px;
+    width: 86px;
+    border-radius: 50%;
+    display: block;
+    margin: 0 auto;
+  }
+  p {
+    font-size: 18px;
+    line-height: 50px;
+    text-align: center;
+    margin: 0;
+  }
+}
 </style>
